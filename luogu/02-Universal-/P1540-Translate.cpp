1869bff6230c1de9f395b8566011becc04bd8ae5
aplqo <aplqo@outlook.com>
2019-11-06 21:56:48 +0800

    Solve 4 luogu problems

---
+++ luogu/02-Universal-/P1540-Translate.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<deque>
+#include<algorithm>
+using namespace std;
+deque<int> mem;
+unsigned int m,ans;
+
+int main()
+{
+    int n,t;
+    cin>>m>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>t;
+        if(find(mem.begin(),mem.end(),t)==mem.end())
+        {
+            if(mem.size()>=m)
+                mem.pop_front();
+            ans++;
+            mem.push_back(t);
+        }
+    }
+    cout<<ans;
+    return 0;
+}
---
+++ luogu/03-Universal/P1056-Seat.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+#define it(v,n) v+1,v+1+n
+
+struct c
+{
+    int num,pos;
+} col[1001],row[1001];
+int m,n,k,l;
+void fun(c ar[],int num)
+{
+	int t=1;
+    for(c *i=ar+1;i<=ar+num;i++,t++)
+    	i->pos=t;
+    
+}
+int main()
+{
+    int d,x,y,i,j;
+    auto cnum=[](const c &a,const c &b)->bool{return a.num>b.num;};
+    auto cpos=[](const c &a,const c &b)->bool{return a.pos<b.pos;};
+    auto pri=[](const c &a){cout<<a.pos<<' ';};
+    cin>>m>>n>>k>>l>>d;
+    fun(col,m);
+    fun(row,n);
+    for(int t=0;t<d;t++)
+    {
+        cin>>y>>x>>j>>i;
+        if(x==i)
+            row[min(y,j)].num++;
+        else if(y==j)
+            col[min(x,i)].num++;
+    }
+    sort(it(col,m),cnum);
+    sort(it(col,l),cpos);
+    sort(it(row,n),cnum);
+    sort(it(row,k),cpos);
+    for_each(it(row,k),pri);
+    cout<<endl;
+    for_each(it(col,l),pri);
+    return 0;
+}
