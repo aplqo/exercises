f62240f759581ab7b432f2bea390a3cd55a6f1f8
aplqo <aplqo@Outlook.com>
2019-11-11 22:02:10 +0800

    Add 2 luogu

---
+++ luogu/03-Universal/P2725-Stamps.cpp
@@ -0,0 +1,30 @@
+#include<algorithm>
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int stamps[200],f[10000*200];
+
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    for(int *i=stamps;i<stamps+k;i++)
+    {
+        cin>>*i;
+        f[*i]=1;
+    }
+    fill(f+1,f+10000*200,INT_MAX);
+    for(int i=1;;i++)
+    {
+        for(int *j=stamps;j<stamps+k;j++)
+            if(*j<=i)
+                f[i]=min(f[i],f[i-*j]+1);
+        if(f[i]>n)
+        {
+            cout<<i-1;
+            return 0;
+        }
+    }
+    return 0;
+}
