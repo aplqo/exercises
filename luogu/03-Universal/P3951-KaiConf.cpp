ed6d6696fcf8286bed8c917106139484e7d3632a
aplqo <aplqo@outlook.com>
2019-11-13 22:00:26 +0800

    Solve 2 luogu:P1057,P3951

---
+++ luogu/03-Universal/P3951-KaiConf.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    unsigned long long a,b;
+    cin>>a>>b;
+    /*
+        ax+by=c
+        x<b,y<0 or x<0,y<a
+        c_max=a(b-1)+b*(-1)=ab-a-b
+    */
+    cout<<(a*b-a-b);
+    return 0;
+}
