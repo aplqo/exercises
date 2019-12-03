b5c6d81bd85dd710d27d0a1333471cc0d15e4b53
aplqo <aplqo@outlook.com>
2019-12-03 22:00:02 +0800

    Solve 2 luogu problems:P1012,P1071

---
+++ luogu/02-Universal-/P1012-Concat.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+const int maxn=25;
+
+string s[25];
+
+int main()
+{
+    int n;
+    cin>>n;
+    for_each(s,s+n,[](string &i){cin>>i;});
+    sort(s,s+n,[](const string &a,const string &b)->bool{return (a+b)>(b+a);});
+    for_each(s,s+n,[](const string &i){cout<<i;});
+    return 0;
+}
---
+++ luogu/03-Universal/P1071-Sink.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdlib>
+#include<string>
+using namespace std;
+
+char pa[26], ci[26];
+
+void err()
+{
+	cout << "Failed" << endl;
+	exit(0);
+}
+int main()
+{
+	string p, c, t;
+	cin >> c >> p >> t;
+	for (unsigned int i = 0; i < p.size(); i++)
+	{
+		unsigned int t1 = c[i] - 'A', t2 = p[i] - 'A';
+		if ((pa[t1] || ci[t2]) && pa[t1] != p[i]) err();
+		pa[t1] = p[i]; ci[t2] = c[i];
+	}
+	if (find(pa, pa + 26, 0) != pa + 26) err();
+	for (unsigned int i = 0; i < t.size(); i++)
+		cout << pa[t[i] - 'A'];
+	return 0;
+}
