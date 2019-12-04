09b052dd33bfb4f22433d2ea171151a5f851fa87
aplqo <aplqo@outlook.com>
2019-12-04 21:58:55 +0800

    Solve 1 luogu problem:P1706

---
+++ luogu/02-Universal-/P1706-Permutation.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool vis[10];
+int n,ans[10];
+void f(int* cur)
+{
+    if(cur==ans+n)
+    {
+        for_each(ans,cur,[](int &i)->void{cout.width(5);cout<<i;});
+        cout<<endl;
+        return;
+    }
+    for(int i=1;i<=n;i++)
+        if(!vis[i])
+        {
+            vis[i]=true;
+            *cur=i;
+            f(cur+1);
+            vis[i]=false;
+        }
+}
+int main()
+{
+    cin>>n;
+    f(ans);
+    return 0;
+}
