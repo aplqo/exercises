e7e39ba845928287d8eea0c8579c867189ee1afd
aplqo <aplqo@outlook.com>
2019-12-10 23:51:01 +0800

    Update debug tools:time

--- tools/time.cpp
+++ tools/time.cpp
@@ -2,29 +2,123 @@
 #include<cstdio>
 #include<cstdlib>
 #include<chrono>
-using std::cout;
-using std::endl;
-using std::chrono::steady_clock;
-using std::chrono::microseconds;
-using std::chrono::milliseconds;
-using std::chrono::duration_cast;
+#include<csignal>
+#include<cstring>
+#include<future>
+#include<string>
+using namespace std;
+using namespace std::chrono;
 
+namespace apdebug
+{
+	inline int run(int argc, char* c[]);
+	unsigned int tlim = 1000, hardlim = tlim * 10;
+	char* cmd[100], ccmd = 1;
+	char testcmd[100], * out = nullptr;
+	/*---Timer---*/
+	template<class tim, class uni1, class uni2>
+	class timer
+	{
+	public:
+		timer(const char* u1, const char* u2, unsigned int l) : n1(u1), n2(u2), lim(l) {};
+		inline void start() { beg = tim::now(); };
+		inline void stop() { aft = tim::now(); };
+		void print()
+		{
+			cerr << endl;
+			uni1 d1 = duration_cast<uni1>(aft - beg);
+			uni2 d2 = duration_cast<uni2>(aft - beg);
+			cerr << "Program finished after " << d1.count() << n1;
+			cerr << " ( " << d2.count() << n2 << " )." << endl;
+			if (d1.count() > lim) cerr << "Warn: TLE may happen" << endl;
+		}
+	private:
+		typename tim::time_point beg, aft;
+		unsigned int lim;
+		const char* n1, * n2;
+	};
+	/*---Signal Handlers---*/
+	extern "C" void sig(int s)
+	{
+		cerr << "RE: Received SIG";
+		switch (s)
+		{
+		case SIGSEGV:
+			cerr << "SEGV"; break;
+		case SIGFPE:
+			cerr << "FPE"; break;
+		case SIGINT:
+			cerr << "INT"; break;
+		case SIGILL:
+			cerr << "ILL"; break;
+		case SIGTERM:
+			cerr << "TERM"; break;
+		}
+		cerr << endl;
+		abort();
+	}
+	void regsig()
+	{
+		signal(SIGSEGV, sig);
+		signal(SIGFPE, sig);
+		signal(SIGILL, sig);
+		signal(SIGINT, sig);
+		signal(SIGTERM, sig);
+	}
+	int call_main()
+	{
+		timer<steady_clock, milliseconds, microseconds> t("ms", "us", tlim);
+		t.start();
+		int ret = run(ccmd, cmd);
+		t.stop(); t.print();
+		return ret;
+	}
+	int test()
+	{
+		if (out == nullptr || strlen(testcmd) == 0) return 0;
+		cerr << "-----Test result-----" << endl;
+		return system(testcmd);
+	}
+}
 int main(int argc, char* argv[])
 {
-	if (argc < 2)
+	apdebug::cmd[0]=argv[0];
+	apdebug::regsig();
+	for (int i = 1; i < argc; ++i)
 	{
-		cout << "Too few arguments" << endl;
-		return 0;
+		if (!strcmp(argv[i], "-in")) freopen(argv[++i], "r", stdin);
+		else if (!strcmp(argv[i], "-out"))
+		{
+			freopen(argv[++i], "w", stdout);
+			apdebug::out = argv[i];
+		}
+		else if (!strcmp(argv[i], "-test")) strcpy(apdebug::testcmd, argv[++i]);
+		else if (!strcmp(argv[i], "-time")) apdebug::tlim = atoi(argv[++i]);
+		else if (!strcmp(argv[i], "-hlimit")) apdebug::hardlim = atoi(argv[++i]);
+		else if (!strcmp(argv[i], "-args"))
+		{
+			apdebug::ccmd += atoi(argv[++i]);
+			for (int j = 1; j <= apdebug::ccmd; ++j, ++i)
+				apdebug::cmd[j] = argv[i];
+		}
 	}
-	freopen(argv[2], "r",stdin);
-	auto bef = steady_clock::now();
-	system(argv[1]);
-	auto aft = steady_clock::now();
-	microseconds d = duration_cast<microseconds>(aft - bef);
-	milliseconds d2=duration_cast<milliseconds>(aft-bef);
-	cout << endl << "---------------" << endl;
-	cout << "Program finished after " << d.count() << "us. ("<<d2.count()<<"ms)";
-	if(d2.count()>=1000)
-		cout<<endl<<"Warn: TLE may happen";
+	future<int> f = async(launch::async,apdebug::call_main);
+	if (f.wait_for(milliseconds(apdebug::hardlim)) == future_status::timeout)
+	{
+		cerr << "MLE/TLE: " << "Hard time limit(" << apdebug::hardlim << ") exceeded.";
+		exit(0);
+	}
+	apdebug::test();
 	return 0;
 }
+#ifndef APINPROG
+inline int apdebug::run(int argc, char* argv[])
+{
+	string cm(argv[0]);
+	for (int i = 1; i < argc; i++)
+		cm += " " + string(argv[i]);
+	return system(cm.c_str());
+}
+#else
+#define main(...) apdebug::run(int argc,char *argv[])
+#endif
