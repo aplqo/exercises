599baf0193c75bf410f6030a9f2df248df3e4944
aplqo <aplqo@outlook.com>
2019-12-11 21:58:30 +0800

    Solve 2 luogu problems:P1605,P2404

---
+++ luogu/02-Universal-/P2404-NPartition.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+const int maxn=9;
+typedef unsigned int numType;
+
+numType seq[maxn]={1},n;
+
+void fun(numType s,numType *cur)
+{
+    if(s==0)
+    {
+        for(numType *i=seq+1;i<cur-1;i++)
+            cout<<*i<<"+";
+        cout<<*(cur-1)<<endl;
+        return;
+    }
+    for(numType i=*(cur-1);i<=s&&i<n;i++)
+    {
+        *cur=i;
+        fun(s-i,cur+1);
+    }
+}
+int main()
+{
+    cin>>n;
+    fun(n,seq+1);
+    return 0;
+}
