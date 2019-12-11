a50507155d2236e0767ce177f42f48c52e80c1cf
aplqo <aplqo@outlook.com>
2019-12-11 13:33:32 +0800

    Solve 2 luogu problems:P1843,P2440

---
+++ luogu/03-Universal/P1843-DryClothes.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+typedef unsigned int resType;
+const int maxn=500005;
+
+unsigned int a,b,n,clo[maxn];
+
+bool test(resType t)
+{
+    resType mt=t,bas=a*t;
+    for(unsigned int *i=clo;i<clo+n;++i)
+    {
+        if(*i>bas)
+        {
+            if((*i-bas)>mt*b) return false;
+            mt-=ceil(double(*i-bas)/b);
+        }
+    }
+    return true;
+}
+int main()
+{
+    cin>>n>>a>>b;
+    for_each(clo,clo+n,[](unsigned int &i)->void{cin>>i;});
+    resType l=0,r=*max_element(clo,clo+n)/a+1;
+    while(l<=r)
+    {
+        resType mid=(l+r)>>1;
+        if(test(mid)) r=mid-1;
+        else l=mid+1;
+    }
+    cout<<l;
+    return 0;
+}
---
+++ luogu/03-Universal/P2440-Process.cpp
@@ -0,0 +1,39 @@
+#ifdef APDEBUG
+#define APINPROG
+#include"time.cpp"
+#endif
+#include<iostream>
+#include<algorithm>
+using namespace std;
+typedef unsigned int numType;
+const int maxn=100005;
+
+numType wod[maxn],k;
+unsigned int n;
+
+bool test(numType d)
+{
+    numType ans=0;
+    for(numType *i=wod;i<wod+n;++i)
+        ans+=(*i)/d;
+    return ans>=k;
+}
+int main()
+{
+    cin>>n>>k;
+    for(numType *i=wod;i<wod+n;++i)
+        cin>>*i;
+    numType l=0,r=*max_element(wod,wod+n);
+    while(l<=r)
+    {
+        numType mid=(l+r)>>1;
+        if(mid==0)
+        {
+            r=l;break;
+        }
+        if(test(mid)) l=mid+1;
+        else r=mid-1;
+    }
+    cout<<r;
+    return 0;
+}
