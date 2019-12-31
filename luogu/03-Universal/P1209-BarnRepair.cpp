5ba291edb2ac886d632082580ef6d8d4e37ead5f
aplqo <aplqo@outlook.com>
2019-12-31 22:17:56 +0800

    Solve 2 luogu problems: P1209,P1325

---
+++ luogu/03-Universal/P1209-BarnRepair.cpp
@@ -0,0 +1,30 @@
+#ifdef APDEBUG
+#define APINPROG
+#define COLOR
+#include "debug_tools/time.cpp"
+#endif
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <numeric>
+using namespace std;
+const int maxn = 205;
+
+int pos[maxn], dif[maxn];
+int m, s, c, ans;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin >> m >> s >> c;
+    m = min(m, c);
+    for (int* i = pos; i < pos + c; ++i)
+        cin >> *i;
+    sort(pos, pos + c);
+    adjacent_difference(pos, pos + c, dif);
+    partial_sort(dif + 1, dif + m, dif + c, greater<int>());
+    ans = pos[c - 1] - pos[0] + 1;
+    for_each(dif + 1, dif + m, [](int t) { ans -= (t - 1); });
+    cout << ans;
+    return 0;
+}
---
+++ luogu/04-Senior/P1325-RaderFix.cpp
@@ -0,0 +1,46 @@
+#ifdef APDEBUG
+#define APINPROG
+#include "debug_tools/time.cpp"
+#endif
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iostream>
+using namespace std;
+const int maxn = 1005;
+
+struct isl
+{
+    int a, b;
+} is[maxn];
+int n, d, ans;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin >> n >> d;
+    for (isl* i = is; i < is + n; ++i)
+    {
+        int x, y, dx;
+        cin >> x >> y;
+        if (y > d)
+        {
+            cout << "-1" << endl;
+            return 0;
+        }
+        dx = sqrt(d * d - y * y);
+        i->a = x - dx;
+        i->b = x + dx;
+    }
+    sort(is, is + n, [](const isl& a, const isl& b) -> bool { return a.b < b.b; });
+    int c = INT_MIN;
+    for (isl* i = is; i < is + n; ++i)
+    {
+        if (i->a <= c)
+            continue;
+        ++ans;
+        c = i->b;
+    }
+    cout << ans;
+    return 0;
+}
