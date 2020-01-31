b3c596ebe6a7538275c1c25e856550cea13521c4
aplqo <aplqo@outlook.com>
2020-01-31 21:51:31 +0800

    Solve 5 luogu problems: P3375,P3805,P3808,P3763,P4503

---
+++ luogu/03-Universal/P3375-KMP.cpp
@@ -0,0 +1,46 @@
+#ifdef APDEBUG
+#include "debug_tools/program.h"
+#endif
+#include <cstring>
+#include <iostream>
+using namespace std;
+const int maxn = 1e6;
+
+char s1[maxn + 10], s2[maxn + 10];
+int l1, l2, nxt[maxn + 10];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> (s1 + 1) >> (s2 + 1);
+    l1 = strlen(s1 + 1);
+    l2 = strlen(s2 + 1);
+
+    int j = 0;
+    for (int i = 1; i <= l2; ++i)
+    {
+        while (j && s2[i + 1] != s2[j + 1])
+            j = nxt[j];
+        if (s2[j + 1] == s2[i + 1])
+            ++j;
+        nxt[i + 1] = j;
+    }
+
+    int k = 0;
+    for (int i = 1; i <= l1; ++i)
+    {
+        while (k && s1[i] != s2[k + 1])
+            k = nxt[k];
+        if (s1[i] == s2[k + 1])
+            ++k;
+        if (k == l2)
+        {
+            cout << i - k + 1 << endl;
+            k = nxt[k];
+        }
+    }
+    for (int i = 1; i <= l2; ++i)
+        cout << nxt[i] << " ";
+    return 0;
+}
---
+++ luogu/05-Senior+/P3808-AhoCorasick.cpp
@@ -0,0 +1,77 @@
+#ifdef APDEBUG
+#include "debug_tools/program.h"
+#endif
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <queue>
+using namespace std;
+const int maxn = 1e6;
+
+int ch[maxn + 10][26], b[maxn + 10], fail[maxn + 10];
+int ans = 0;
+char tmp[maxn + 10];
+
+void add(const char* str)
+{
+    static int cur = 0;
+    size_t l = strlen(str);
+    int k = 0;
+    for (size_t i = 0; i < l; ++i)
+    {
+        int c = str[i] - 'a';
+        if (!ch[k][c])
+            ch[k][c] = ++cur;
+        k = ch[k][c];
+    }
+    ++b[k];
+}
+void make()
+{
+    queue<int> q;
+    for (int i = 0; i < 26; ++i)
+        if (ch[0][i])
+            q.push(ch[0][i]);
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+        for (int i = 0; i < 26; ++i)
+        {
+            if (!ch[cur][i])
+                ch[cur][i] = ch[fail[cur]][i];
+            else
+            {
+                fail[ch[cur][i]] = ch[fail[cur]][i];
+                q.push(ch[cur][i]);
+            }
+        }
+    }
+}
+void find(const char* str)
+{
+    size_t l = strlen(str);
+    int k = 0;
+    for (size_t i = 0; i < l; ++i)
+    {
+        k = ch[k][str[i] - 'a'];
+        ans += b[k];
+        b[k] = 0;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> tmp;
+        add(tmp);
+    }
+    make();
+    cin >> tmp;
+    find(tmp);
+    cout << ans << endl;
+    return 0;
+}
