3dbbf7ee2bad437ad6058fa4c5621c3caf9c4f3c
aplqo <aplqo@outlook.com>
2020-02-04 21:37:13 +0800

    Add exam problems: box,desert,revegetate

---
+++ others/box.cpp
@@ -0,0 +1,38 @@
+#ifdef APDEBUG
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <queue>
+using namespace std;
+const int maxn = 5000;
+
+unsigned int w[maxn + 10], n;
+// clang-format off
+priority_queue<unsigned int, vector<unsigned int>, greater<unsigned int> > heap;
+// clang-format on
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;
+    for (unsigned int* i = w; i < w + n; ++i)
+        cin >> *i;
+    sort(w, w + n);
+    heap.push(1);
+    for (unsigned int* i = w + 1; i < w + n; ++i)
+    {
+        if (heap.top() > *i)
+            heap.push(1);
+        else
+        {
+            unsigned int nxt = heap.top() + 1;
+            heap.pop();
+            heap.push(nxt);
+        }
+    }
+    cout << heap.size() << endl;
+    return 0;
+}
