470990d81e32cbbe586ade629b04e1d3319d4364
aplqo <aplqo@outlook.com>
2020-03-16 21:55:46 +0800

    Correct 2020.03.15 exam

--- others/2020.03/2020.03.15-Exam/A-Palace.cpp
+++ others/2020.03/2020.03.15-Exam/A-Palace.cpp
@@ -3,13 +3,11 @@
 #endif
 #include <iostream>
 using namespace std;
-using num_t = long long;
-constexpr num_t mod = 1e9 + 7;
+using num_t = unsigned long long;
+constexpr num_t mod = 1e8 + 7; // problem
 
 num_t fact(const unsigned int n)
 {
-    if (!n)
-        return 0;
     num_t ret = 1;
     for (unsigned int i = 1; i <= n; ++i)
         ret = (ret * i) % mod;
@@ -21,13 +19,27 @@ int main()
     ios_base::sync_with_stdio(false);
 #endif
     unsigned int n;
-    num_t ans, tmp = 0;
     cin >> n;
-    ans = fact(n) % mod;
-    for (unsigned int i = 1; i < n; ++i)
-        tmp = (tmp + i * fact(n - i)) % mod;
-    ans = (ans - tmp + mod) % mod;
-    ans = (ans * n) % mod;
-    cout << ans << endl;
+
+    num_t d1 = 0, d2 = 1;
+    for (unsigned int i = 3; i <= n; ++i)
+    {
+        const num_t tmp = ((i - 1) * ((d1 + d2) % mod)) % mod;
+        d1 = d2;
+        d2 = tmp;
+    }
+
+    switch (n)
+    {
+    case 1:
+        cout << "orzlxx" << endl;
+        break;
+    case 2:
+        cout << d2 * fact(2) << endl;
+        break;
+    default:
+        cout << (d2 * fact(n)) % mod << endl;
+        break;
+    }
     return 0;
 }
