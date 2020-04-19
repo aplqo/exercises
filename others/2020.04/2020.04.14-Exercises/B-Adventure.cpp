1630a23d167d1187469633ec6bed543a4e0ee461
aplqo <aplqo@outlook.com>
2020-04-19 10:01:58 +0800

    Correct 2020.04.14 exercises: B-T129624

--- others/2020.04/2020.04.14-Exercises/B-Adventure.cpp
+++ others/2020.04/2020.04.14-Exercises/B-Adventure.cpp
@@ -10,58 +10,54 @@
 #include <deque>
 #include <iostream>
 #include <limits>
+#include <utility>
 using namespace std;
-constexpr double ninf = numeric_limits<double>::min();
-const int maxn = 1000;
+const int maxn = 100000;
 
-long long grt[maxn + 10], val[maxn + 10];
-double f[maxn + 10][maxn + 10], po[maxn + 10] = { 1 };
+long long val[maxn + 10];
+double f[maxn + 10];
 
-void init(const unsigned int n, const unsigned int m, const double k)
+class myque
 {
-    for (unsigned int i = 1; i <= n; ++i)
-        po[i] = po[i - 1] * k;
-    for (unsigned int i = 0; i < m; ++i)
-        fill(f[i] + 1, f[i] + 1 + n, ninf);
-    for (unsigned int i = m; i <= n; ++i)
-        fill(f[i], f[i] + 1 + n, ninf);
+public:
+    void push(const unsigned int i, const double val)
     {
-        deque<unsigned int> q;
-        const auto push = [&q](const unsigned int p) -> void {
-            while (!q.empty() && val[q.back()] < val[p])
-                q.pop_back();
-            q.emplace_back(p);
-        };
-        const auto pop = [&q](const unsigned int p) -> void {
-            while (!q.empty() && q.front() < p)
-                q.pop_front();
-        };
-        for (unsigned int i = 1; i <= m; ++i)
-            push(i);
-        for (unsigned int i = 1; i <= n - m + 1; ++i)
-        {
-            pop(i);
-            push(i + m - 1);
-            grt[i] = val[q.front()];
-        }
+        while (!q.empty() && q.back().second <= val)
+            q.pop_back();
+        q.emplace_back(i, val);
     }
-}
-double dp(const unsigned int n, const unsigned int m)
+    void pop(const unsigned int p)
+    {
+        while (!q.empty() && q.front().first > p)
+            q.pop_front();
+    }
+    double front() const
+    {
+        return q.front().second;
+    }
+
+private:
+    deque<pair<unsigned int, double>> q;
+};
+
+double dp(const unsigned int n, const unsigned int m, const double k)
 {
-    for (unsigned int i = 1; i < m; ++i)
-        for (unsigned int j = 1; j <= i; ++j)
-            f[i][j] = max(f[i - 1][j], f[i - 1][j - 1] + val[i] * po[j - 1]);
-    for (unsigned int i = m; i <= n; ++i)
-        for (unsigned int j = ceil(1.0 * i / m); j <= i; ++j)
-        {
-            if (grt[i - m + 1] > 0)
-                f[i][j] = max(f[i - 1][j], f[i - 1][j - 1] + val[i] * po[j - 1]);
-            else
-                f[i][j] = f[i - m][j - 1] + grt[i - m] * po[i - 1];
-        }
-    double ret = ninf;
-    for (unsigned int i = ceil(1.0 * n / m); i <= n; ++i)
-        ret = max(ret, f[n][i]);
+    myque q;
+    q.push(n, val[n]);
+    for (unsigned int i = n - 1; i > n - m; --i)
+    {
+        f[i] = max<double>(q.front() * k + val[i], val[i]);
+        q.push(i, f[i]);
+    }
+    for (unsigned int i = n - m; i; --i)
+    {
+        q.pop(i + m);
+        f[i] = q.front() * k + val[i];
+        q.push(i, f[i]);
+    }
+    double ret = numeric_limits<double>::lowest();
+    for (unsigned int i = 1; i <= m; ++i)
+        ret = max(ret, f[i]);
     return ret;
 }
 int main()
@@ -71,7 +67,6 @@ int main()
     cin >> n >> m >> k;
     for (long long* i = val + 1; i < val + 1 + n; ++i)
         cin >> *i;
-    init(n, m, k);
-    printf("%.2lf\n", dp(n, m));
+    printf("%.2lf\n", dp(n, m, k));
     return 0;
 }
