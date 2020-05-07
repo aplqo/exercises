20c47447371a8fc18121c22479b7320e610db2ae
aplqo <aplqo@outlook.com>
2020-05-07 22:52:34 +0800

    Correct 2020.05.06 exercises: A-T132648

--- others/2020.05/2020.05.06-Exercises/A-CrazyOI.cpp
+++ others/2020.05/2020.05.06-Exercises/A-CrazyOI.cpp
@@ -4,39 +4,74 @@
 #endif
 #include <algorithm>
 #include <iostream>
-#include <utility>
+#ifdef LogGCD
+#include <fstream>
+using std::ofstream;
+ofstream glog;
+#define writeGcdLog(c, a, m, x) glog << (c) << " " << (a) << " " << (m) << " " << (x) << endl;
+#define openGcdLog(argc, argv) \
+    if (argc > 0)              \
+        glog.open(argv[1]);
+#else
+#define writeGcdLog(c, a, m, x)
+#define openGcdLog(argc, argv)
+#endif
 using namespace std;
-const int maxm = 500000, maxn = 12;
+using num = unsigned int;
+using gcd_t = int;
+const int maxm = 1e6, maxn = 15;
 
-unsigned int c[maxn + 10], p[maxn + 10], l[maxn + 10];
-unsigned int vis[maxm + 10], pos[maxn + 10];
-unsigned int n, ml;
+num c[maxn + 10], p[maxn + 10], l[maxn + 10];
+unsigned int n;
 
-bool test(const unsigned int m)
+gcd_t exgcd(gcd_t a, gcd_t b, gcd_t& x, gcd_t& y)
 {
-    static unsigned int sta = 0;
-    copy(c, c + n, pos);
-    for (unsigned int i = 0; i <= ml; ++i, ++sta)
+    if (b == 0)
     {
-        ++sta;
-        for (unsigned int j = 0; j < n; ++j)
-        {
-            if (i > l[j])
-                continue;
-            if (vis[pos[j]] == sta)
-                return false;
-            vis[pos[j]] = sta;
-            pos[j] = (pos[j] + p[j]) % m;
-        }
+        x = 1;
+        y = 0;
+        return a;
     }
+    const gcd_t g = exgcd(b, a % b, x, y);
+    gcd_t t = x;
+    x = y;
+    y = t - (a / b) * y;
+    return g;
+}
+num solve(const unsigned int i, const unsigned int j, const gcd_t m)
+{
+    const gcd_t a = (gcd_t)p[j] - (gcd_t)p[i], cc = (gcd_t)c[i] - (gcd_t)c[j];
+    gcd_t x, y;
+    gcd_t g = a > m ? exgcd(a, m, x, y) : exgcd(m, a, y, x);
+    if (g < 0)
+        g = -g;
+    if (cc % g)
+        return maxm + 10;
+    x *= cc / g;
+    if (x < 0)
+        x += ((-x + m / g) / (m / g)) * (m / g);
+    x %= m / g;
+    writeGcdLog(cc, a, m, x);
+    return x;
+}
+bool test(const num m)
+{
+    for (unsigned int i = 0; i < n; ++i)
+        for (unsigned int j = i + 1; j < n; ++j)
+        {
+            const unsigned int p = solve(i, j, m);
+            if (p <= l[i] && p <= l[j])
+                return false;
+        }
     return true;
 }
-int main()
+int main(int argc, char* argv[])
 {
 #ifndef APTEST
     ios_base::sync_with_stdio(false);
 #endif
-    unsigned int lv = 0, rv = maxm;
+    openGcdLog(argc, argv);
+    num lv = 0, rv = maxm;
     cin >> n;
     for (unsigned int i = 0; i < n; ++i)
     {
@@ -44,7 +79,6 @@ int main()
         --c[i];
     }
     lv = *max_element(c, c + n);
-    ml = *max_element(l, l + n);
     ++lv;
     for (; lv <= maxm; ++lv)
         if (test(lv))
