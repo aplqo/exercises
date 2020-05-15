5560d10835b8512e675de2a427e2affbf4a3cb51
aplqo <aplqo@outlook.com>
2020-05-15 21:12:39 +0800

    Solve 2 poj problems:2369,3070

---
+++ poj/2369-Permutations.cpp
@@ -0,0 +1,55 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <iostream>
+using namespace std;
+const int maxn = 1000;
+typedef long long ans_t;
+
+unsigned int to[maxn + 10], ring[maxn + 10];
+unsigned int len[maxn + 10];
+
+ans_t gcd(ans_t a, ans_t b)
+{
+    if (a < b)
+        swap(a, b);
+    while (b)
+    {
+        const ans_t t = a;
+        a = b;
+        b = t % b;
+    }
+    return a;
+}
+ans_t lcm(const ans_t a, const ans_t b)
+{
+    return a * b / gcd(a, b);
+}
+void find(unsigned int x, const unsigned int rn)
+{
+    while (!ring[x])
+    {
+        ring[x] = rn;
+        ++len[rn];
+        x = to[x];
+    }
+}
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    unsigned int n, cr = 0;
+    ans_t ans = 1;
+    cin >> n;
+    for (unsigned int* i = to + 1; i < to + n + 1; ++i)
+        cin >> *i;
+    for (unsigned int i = 1; i <= n; ++i)
+        if (!ring[i])
+            find(i, ++cr);
+    for (unsigned int i = 1; i <= cr; ++i)
+        ans = lcm(ans, len[i]);
+    cout << ans << endl;
+    return 0;
+}
---
+++ poj/3070-Fibonacci.cpp
@@ -0,0 +1,121 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef unsigned int num;
+const num mod = 1e4;
+
+class Number
+{
+public:
+    Number()
+        : val(0)
+    {
+    }
+    Number(num i)
+        : val(i % mod)
+    {
+    }
+#define decl(x)                                                      \
+    inline friend Number operator x(const Number l, const Number r); \
+    inline Number operator x##=(const Number r) { return *this = *this x r; }
+    decl(*);
+    decl(+);
+#undef decl
+    inline friend ostream& operator<<(ostream& os, const Number r);
+
+private:
+    num val;
+};
+#define impl(x) \
+    inline Number operator x(const Number l, const Number r) { return l.val x r.val; }
+impl(*);
+impl(+);
+#undef impl
+inline ostream& operator<<(ostream& os, const Number r)
+{
+    return os << r.val;
+}
+struct vec
+{
+    vec() {}
+    vec(unsigned int x, unsigned int y)
+    {
+        val[0] = x;
+        val[1] = y;
+    }
+    inline Number& operator[](const unsigned int i)
+    {
+        return val[i];
+    }
+    inline Number operator[](const unsigned int i) const
+    {
+        return val[i];
+    }
+    Number val[2];
+};
+struct matrix
+{
+    matrix()
+    {
+        val[0] = { 1, 0 };
+        val[1] = { 0, 1 };
+    }
+    vec operator*(const vec r) const
+    {
+        vec ret(0, 0);
+        for (unsigned int i = 0; i < 2; ++i)
+            for (unsigned int j = 0; j < 2; ++j)
+                ret[j] += r[i] * val[i][j];
+        return ret;
+    }
+    matrix operator*(const matrix r) const
+    {
+        matrix ret;
+        ret.val[0] = { 0, 0 };
+        ret.val[1] = { 0, 0 };
+        for (unsigned int i = 0; i < 2; ++i)
+            ret.val[i] = r * val[i];
+        return ret;
+    }
+    matrix operator^(unsigned int e) const
+    {
+        matrix ret, v = *this;
+        for (unsigned int i = 1; e; i <<= 1)
+        {
+            if (e & i)
+            {
+                ret = ret * v;
+                e ^= i;
+            }
+            v = v * v;
+        }
+        return ret;
+    }
+
+    vec val[2];
+};
+
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    matrix m;
+    m.val[0] = { 0, 1 };
+    m.val[1] = { 1, 1 };
+    while (true)
+    {
+        int n;
+        cin >> n;
+        if (n == -1)
+            break;
+        if (!n)
+            cout << "0" << endl;
+        else
+            cout << ((m ^ (n - 1)) * vec(0, 1))[1] << endl;
+    }
+    return 0;
+}
