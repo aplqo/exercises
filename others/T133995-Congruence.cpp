710722f6c79f3f21829340ad5f4fd6fc46923e3d
aplqo <aplqo@outlook.com>
2020-05-28 23:01:01 +0800

    [others] Solve 2 problem: T133995,T134710

---
+++ others/T133995-Congruence.cpp
@@ -0,0 +1,38 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <iostream>
+using namespace std;
+
+void exgcd(const long long a, const long long b, long long& x, long long& y)
+{
+    if (!b)
+    {
+        x = 1;
+        y = 0;
+        return;
+    }
+    exgcd(b, a % b, x, y);
+    long long t = x;
+    x = y;
+    y = t - (a / b) * y;
+}
+long long solve(const long long a, const long long b)
+{
+    long long x, y;
+    exgcd(a, b, x, y);
+    if (x < 0)
+        x += (-x + b) / b * b;
+    x %= b;
+    return x;
+}
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    long long a, b;
+    cin >> a >> b;
+    cout << solve(a, b) << endl;
+    return 0;
+}
--- others/T134710-FoolGitf.cpp
+++ others/T134710-FoolGitf.cpp
@@ -20,7 +20,7 @@ struct
     int x, y;
 } chl[maxn + 1];
 real dis[maxn + 1][maxn + 1];
-real f[maxn][1 << maxn][maxn], cycle[1 << maxn];
+real f[maxn][1 << maxn][4], cycle[1 << maxn];
 unsigned int lg[1 << maxn], pcnt[1 << maxn];
 
 unsigned int popcnt(unsigned int n)
@@ -38,15 +38,13 @@ void init(const unsigned int n)
     for (unsigned int i = 0; i <= n; ++i)
         for (unsigned int j = i + 1; j <= n; ++j)
             dis[i][j] = dis[j][i] = hypot(chl[i].x - chl[j].x, chl[i].y - chl[j].y);
-    for (unsigned int i = 0; i < (1 << n); ++i)
-        pcnt[i] = popcnt(i);
     for (unsigned int i = 0; i <= n; ++i)
         lg[1 << i] = i;
     for (unsigned int i = 0; i < (1 << n); ++i)
         for (unsigned int j = i; j; j &= j - 1)
         {
             const unsigned int p = lg[lowbit(j)];
-            fill(f[p][i], f[p][i] + pcnt[i], inf);
+            fill(f[p][i], f[p][i] + 4, inf);
         }
     fill(cycle + 1, cycle + (1 << n), inf);
 }
@@ -55,18 +53,21 @@ unsigned int dp(const unsigned int n, const real v)
     const unsigned int msk = (1 << n) - 1;
     for (unsigned int i = 0; i < (1 << n); ++i)
     {
+        const unsigned int mc = min(3u, popcnt(i)) + 1;
         for (unsigned int j = i; j; j &= j - 1)
         {
             const unsigned int from = lg[lowbit(j)];
-            for (unsigned int c = 1; c <= pcnt[i]; ++c)
+            for (unsigned int c = 1; c <= mc; ++c)
             {
                 const real cv = v / (1 << c);
+                cycle[i] = min(cycle[i], f[from][i][c - 1] + dis[from + 1][0] / cv);
+                if (c == 3)
+                    continue;
                 for (unsigned int k = (~i) & msk; k; k &= k - 1)
                 {
                     const unsigned int lb = lowbit(k), to = lg[lb];
                     f[to][i | lb][c] = min(f[to][i | lb][c], f[from][i][c - 1] + dis[from + 1][to + 1] / cv);
                 }
-                cycle[i] = min(cycle[i], f[from][i][c - 1] + dis[from + 1][0] / cv);
             }
         }
         for (unsigned int j = (~i) & msk; j; j &= j - 1)
