900ca3a5956ce9d2a00e04b5668d308d7033661b
aplqo <aplqo@outlook.com>
2020-05-31 11:36:10 +0800

    [exam] Correct 2020.05.30 exam: A-T134986

--- others/2020.05/2020.05.30-Exam/T134986-Gee.cpp
+++ others/2020.05/2020.05.30-Exam/T134986-Gee.cpp
@@ -6,52 +6,40 @@
 #include <iostream>
 #include <numeric>
 using namespace std;
+#define lowbit(x) ((x) & -(x))
 const unsigned int maxn = 1024, maxm = 1000;
 
 char gene[maxn][maxm + 10];
 char root[maxm + 10];
-unsigned int cnt[26];
+unsigned int sol[maxn], lg[(1 << 25) + 1];
 
-char findMax(const unsigned int n, const unsigned int pos)
+void init()
 {
-    memset(cnt, 0, sizeof(cnt));
-    for (unsigned int i = 0; i < n; ++i)
-        ++cnt[gene[i][pos] - 'A'];
-    char ret;
-    unsigned int mv = 0;
-    for (unsigned int i = 0; i < 26; ++i)
-        if (cnt[i] > mv)
-        {
-            mv = cnt[i];
-            ret = 'A' + i;
-        }
-    return ret;
+    for (unsigned int i = 1; i <= 25; ++i)
+        lg[1 << i] = i;
 }
 unsigned int solve(const unsigned int n, const unsigned int pos)
 {
-    char tar;
-    tar = root[pos] = findMax(n, pos);
+    memset(sol, 0, sizeof(sol));
     unsigned int ret = 0;
+    for (unsigned int i = 0; i < n; ++i)
+        sol[i] = 1 << (gene[i][pos] - 'A');
     for (unsigned int nxt = 1; nxt < n; nxt <<= 1)
     {
         const unsigned int step = nxt << 1;
         for (unsigned int i = 0; i + nxt < n; i += step)
         {
-            const char ano = gene[i + nxt][pos], cur = gene[i][pos];
-            if (cur == ano)
-                continue;
-            else if (cur == tar || ano == tar)
-            {
-                ++ret;
-                gene[i][pos] = tar;
-            }
+            const unsigned int cur = sol[i], ano = sol[i + nxt];
+            if (cur & ano)
+                sol[i] &= ano;
             else
             {
                 ++ret;
-                gene[i][pos] = cnt[ano] > cnt[cur] ? ano : cur;
+                sol[i] |= ano;
             }
         }
     }
+    root[pos] = 'A' + lg[lowbit(sol[0])];
     return ret;
 }
 int main()
@@ -59,6 +47,7 @@ int main()
 #ifndef APTEST
     ios_base::sync_with_stdio(false);
 #endif
+    init();
     unsigned int n, m;
     cin >> n >> m;
     for (unsigned int i = 0; i < n; ++i)
