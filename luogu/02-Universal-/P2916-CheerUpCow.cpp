faa4bacfefb2e0f01d431964c9ff9a1dde677e82
aplqo <aplqo@outlook.com>
2020-07-19 21:55:05 +0800

    [lougu] Add 12 problems: P2896,P2916,P3919,P4054,P5017,P1712,P3648,P3755,P3834,P4097,P4104,P4158

---
+++ luogu/02-Universal-/P2916-CheerUpCow.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<algorithm>
+#include<iterator>
+#include<numeric>
+using namespace std;
+const unsigned int maxp=100000,maxn=10000;
+const unsigned int maxv=maxn,maxe=maxp;
+
+struct edge
+{
+	unsigned int from,to;
+	unsigned int val;
+}ed[maxe+1];
+unsigned int c[maxn+1];
+unsigned int fa[maxv+1],siz[maxv+1];
+
+static void init(const unsigned int n)
+{
+	fill(siz+1,siz+1+n,1);
+	iota(fa,fa+1+n,0);
+}
+unsigned int getFather(const unsigned int x)
+{
+	return fa[x]==x? x: fa[x]=getFather(fa[x]);
+}
+inline bool tryMerge(const unsigned int x,const unsigned int y)
+{
+	unsigned int fx=getFather(x),fy=getFather(y);
+	if(fx==fy)
+		return false;
+	if(siz[fx]<siz[fy])
+		swap(fx,fy);
+	fa[fy]=fx;
+	siz[fx]+=siz[fy];
+	return true;
+}
+unsigned int kruskal(const unsigned int n,const unsigned int p)
+{
+	sort(ed,ed+p,[](const edge&a,const edge&b){return a.val<b.val;});
+	const edge*ptr=ed;
+	unsigned int ret=0;
+	for(unsigned int i=1;i<n;++i)
+	{
+		while(!tryMerge(ptr->from,ptr->to))
+			++ptr;
+		ret+=ptr->val;
+	}
+	return ret;
+}
+
+int main()
+{
+#ifndef APTEST
+	ios_base::sync_with_stdio(false);
+#endif
+	unsigned int n,p;
+	cin>>n>>p;
+	copy_n(istream_iterator<unsigned int>(cin),n,c+1);
+	for(edge*i=ed;i<ed+p;++i)
+	{
+		cin>>i->from>>i->to>>i->val;
+		i->val=c[i->from]+c[i->to]+2*(i->val);
+	}
+	init(n);
+	cout<<(kruskal(n,p)+*min_element(c+1,c+1+n))<<endl;
+	return 0;
+}
---
+++ luogu/05-Senior+/P3919-PersistentSegmentTree.cpp
@@ -0,0 +1,127 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <iterator>
+#include <sstream>
+using namespace std;
+const unsigned int maxn = 1e6;
+
+template <class T>
+T read()
+{
+    char c = getchar();
+    T ret = 0, r = 1;
+    while (!isdigit(c) && c != '-')
+        c = getchar();
+    if (c == '-')
+    {
+        r = -1;
+        c = getchar();
+    }
+    while (isdigit(c))
+    {
+        ret = ret * 10 + c - '0';
+        c = getchar();
+    }
+    return ret * r;
+}
+class STree
+{
+public:
+    void build(const unsigned int l, const unsigned int r, int dat[])
+    {
+        if (r - l == 1)
+        {
+            val = dat[l];
+            return;
+        }
+        STree* c = create(l, mv);
+        c->build(l, mv, dat);
+        lc = c;
+        c = create(mv, r);
+        c->build(mv, r, dat);
+        rc = c;
+    }
+    void assign(const STree* old, const unsigned int x, const int val)
+    {
+        if (rv - lv == 1)
+        {
+            this->val = val;
+            return;
+        }
+        if (x < mv)
+        {
+            rc = old->rc;
+            STree* c = create(lv, mv);
+            c->assign(old->lc, x, val);
+            lc = c;
+        }
+        else
+        {
+            lc = old->lc;
+            STree* c = create(mv, rv);
+            c->assign(old->rc, x, val);
+            rc = c;
+        }
+    }
+    int find(const unsigned int x) const
+    {
+        if (rv - lv == 1)
+            return val;
+        return x < mv ? lc->find(x) : rc->find(x);
+    }
+    static STree* create(const unsigned int l, const unsigned int r)
+    {
+        ++cur;
+        cur->lv = l;
+        cur->rv = r;
+        cur->mv = (l + r) >> 1;
+        return cur;
+    }
+
+private:
+    static STree* cur;
+
+    unsigned int lv, mv, rv;
+    int val;
+    const STree *lc, *rc;
+} SNodes[maxn * 4 + maxn * 10 * 4];
+STree* STree::cur = SNodes - 1;
+const STree* root[maxn + 1];
+int seq[maxn + 1];
+
+int main()
+{
+    const unsigned int n = read<unsigned int>();
+    const unsigned int m = read<unsigned int>();
+    generate_n(seq + 1, n, read<int>);
+    ostringstream os;
+    {
+        STree* c = STree::create(1, n + 1);
+        c->build(1, n + 1, seq);
+        root[0] = c;
+    }
+    for (unsigned int i = 1; i <= m; ++i)
+    {
+        const unsigned int v = read<unsigned int>();
+        const unsigned int op = read<unsigned int>();
+        const unsigned int x = read<unsigned int>();
+        if (op == 1)
+        {
+            const int val = read<int>();
+            STree* c = STree::create(1, n + 1);
+            c->assign(root[v], x, val);
+            root[i] = c;
+        }
+        else
+        {
+            root[i] = root[v];
+            os << root[v]->find(x) << endl;
+        }
+    }
+    puts(os.str().c_str());
+    return 0;
+}
