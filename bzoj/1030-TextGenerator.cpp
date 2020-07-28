044c1c1254a6bec020771ec2bc6ce08b7d521b84
aplqo <aplqo@outlook.com>
2020-07-28 21:49:20 +0800

    [bzoj] Add 5 problems: 1030,2140,4010,4398,4887

---
+++ bzoj/1030-TextGenerator.cpp
@@ -0,0 +1,97 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <cstring>
+#include <iostream>
+#include <queue>
+using namespace std;
+const unsigned int maxn = 60, maxl = 100, maxc = 26;
+const unsigned long long mod = 1e4 + 7;
+
+unsigned int ch[maxn * maxl + 1][maxc], fail[maxn * maxl + 1];
+unsigned int cur = 0;
+unsigned long long f[maxn * maxl + 1][maxl + 1];
+bool b[maxn * maxl + 1];
+
+void add(const char* s)
+{
+    const size_t l = strlen(s);
+    unsigned int pos = 0;
+    for (unsigned int i = 0; i < l; ++i)
+    {
+        const unsigned int c = s[i] - 'A';
+        if (!ch[pos][c])
+            ch[pos][c] = ++cur;
+        pos = ch[pos][c];
+    }
+    b[pos] = true;
+}
+void make()
+{
+    queue<unsigned int> q;
+    for (unsigned int i = 0; i < maxc; ++i)
+        if (ch[0][i])
+            q.push(ch[0][i]);
+    while (!q.empty())
+    {
+        const unsigned int cur = q.front();
+        q.pop();
+        for (unsigned int i = 0; i < maxc; ++i)
+        {
+            if (!ch[cur][i])
+                ch[cur][i] = ch[fail[cur]][i];
+            else
+            {
+                fail[ch[cur][i]] = ch[fail[cur]][i];
+                b[ch[cur][i]] |= b[fail[ch[cur][i]]];
+                q.push(ch[cur][i]);
+            }
+        }
+    }
+}
+unsigned long long quickPow(unsigned long long v, unsigned int e)
+{
+    unsigned long long ret = 1;
+    for (; e; e >>= 1)
+    {
+        if (e & 0x01)
+            ret = (ret * v) % mod;
+        v = (v * v) % mod;
+    }
+    return ret;
+}
+unsigned long long dp(const unsigned int m)
+{
+    f[0][0] = 1;
+    for (unsigned int j = 0; j < m; ++j)
+        for (unsigned int i = 0; i <= cur; ++i)
+        {
+            if (b[i])
+                continue;
+            for (unsigned int k = 0; k < maxc; ++k)
+                if (!b[ch[i][k]])
+                    f[ch[i][k]][j + 1] = (f[ch[i][k]][j + 1] + f[i][j]) % mod;
+        }
+    unsigned long long ret = 0;
+    for (unsigned int i = 0; i <= cur; ++i)
+        ret = (ret + f[i][m]) % mod;
+    return ret;
+}
+
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    unsigned int n, m;
+    cin >> n >> m;
+    for (unsigned int i = 0; i < n; ++i)
+    {
+        char str[maxl + 10];
+        cin >> str;
+        add(str);
+    }
+    make();
+    cout << (quickPow(26, m) + mod - dp(m)) % mod << endl;
+    return 0;
+}
---
+++ bzoj/4887-Coke.cpp
@@ -0,0 +1,83 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <iostream>
+#include <valarray>
+using namespace std;
+const unsigned int maxn = 30;
+constexpr unsigned long long mod = 2017;
+
+unsigned int n;
+using Vector = valarray<unsigned long long>;
+struct Matrix
+{
+    Matrix()
+    {
+        for (unsigned int i = 0; i <= n; ++i)
+            val[i].resize(n + 2);
+    }
+    Vector operator*(const Vector& v) const;
+    Matrix operator*(const Matrix& r) const;
+    Matrix operator^(unsigned int e) const;
+    void unit();
+
+    Vector val[maxn + 1];
+};
+Vector Matrix::operator*(const Vector& v) const
+{
+    Vector ret(n + 2);
+    for (unsigned int i = 0; i <= n; ++i)
+        ret = (ret + (v[i] * val[i]) % mod) % mod;
+    return ret;
+}
+Matrix Matrix::operator*(const Matrix& r) const
+{
+    Matrix ret;
+    for (unsigned int i = 0; i <= n; ++i)
+        ret.val[i] = *this * r.val[i];
+    return ret;
+}
+void Matrix::unit()
+{
+    for (unsigned int i = 0; i <= n; ++i)
+        val[i][i] = 1;
+}
+Matrix Matrix::operator^(unsigned int e) const
+{
+    Matrix ret, c = *this;
+    ret.unit();
+    for (; e; e >>= 1)
+    {
+        if (e & 0x01)
+            ret = ret * c;
+        c = c * c;
+    }
+    return ret;
+}
+
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    unsigned int m, t;
+    cin >> n >> m;
+    Matrix ma;
+    ma.unit();
+    for (unsigned int i = 0; i < m; ++i)
+    {
+        unsigned int u, v;
+        cin >> u >> v;
+        ma.val[u][v] = 1;
+        ma.val[v][u] = 1;
+    }
+    for (unsigned int i = 1; i <= n; ++i)
+        ma.val[i][0] = 1;
+    cin >> t;
+    Matrix&& r = ma ^ t;
+    unsigned long long ret = 0;
+    for (unsigned int i = 0; i <= n; ++i)
+        ret = (ret + r.val[1][i]) % mod;
+    cout << ret << endl;
+    return 0;
+}
