2421305d9d548d25f63ca4fac6344750b7072ada
aplqo <aplqo@outlook.com>
2020-10-30 22:36:54 +0800

    [codeforces] Add 3 problems: 1016C, 778D, 936C

---
+++ codeforces/1016C-VasyaAndMushrooms.cpp
@@ -0,0 +1,58 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+using namespace std;
+const unsigned int maxn = 3e5;
+
+enum
+{
+    Pre,
+    Suf
+};
+unsigned long long val[2][maxn + 10], sum[2][maxn + 10];
+unsigned long long gain[2][maxn + 10][2];
+
+template <unsigned int typ>
+static void initGain(const unsigned int n)
+{
+    for (unsigned int i = n; i; --i)
+    {
+        gain[typ][i][Suf] = gain[typ][i + 1][Suf] + val[typ][i] * (n + 1 - i);
+        gain[typ][i][Pre] = gain[typ][i + 1][Pre] + sum[typ][i + 1] + val[typ][i];
+    }
+}
+static void initSum(const unsigned int n, const unsigned long long a[], unsigned long long res[])
+{
+    for (unsigned int i = n; i > 0; --i)
+        res[i] = res[i + 1] + a[i];
+}
+unsigned long long findMax(const unsigned int n)
+{
+    unsigned long long ret = 0, acc = 0;
+    for (unsigned int i = 0; i < n * 2; ++i)
+    {
+        const unsigned int x = (i & 0x03) == 1 || (i & 0x03) == 2, y = i / 2 + 1, typ = i & 0x01;
+        acc += val[x][y] * i;
+        ret = max(ret, acc + i * sum[x][y + 1] + gain[x][y + 1][Pre] + (n + i - y) * sum[!x][y + typ] + gain[!x][y + typ][Suf]);
+    }
+    return ret;
+}
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    unsigned int n;
+    cin >> n;
+    copy_n(istream_iterator<unsigned int>(cin), n, val[0] + 1);
+    copy_n(istream_iterator<unsigned int>(cin), n, val[1] + 1);
+    initSum(n, val[0], sum[0]);
+    initSum(n, val[1], sum[1]);
+    initGain<0>(n);
+    initGain<1>(n);
+    cout << findMax(n) << "\n";
+    return 0;
+}
---
+++ codeforces/936C-LockPuzzle.cpp
@@ -0,0 +1,74 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+using namespace std;
+const unsigned int maxn = 2000;
+
+string s, t;
+vector<unsigned int> ans;
+
+void shift(const unsigned int x)
+{
+    if (!x)
+        return;
+    ans.push_back(x);
+    reverse(s.end() - x, s.end());
+    rotate(s.begin(), s.end() - x, s.end());
+}
+bool checkAnswer(const string& s, const string& t)
+{
+    static unsigned int cnt[2][26];
+    for (unsigned int i = 0; i < s.length(); ++i)
+    {
+        ++cnt[0][s[i] - 'a'];
+        ++cnt[1][t[i] - 'a'];
+    }
+    return equal(cnt[0], cnt[0] + 26, cnt[1]);
+}
+template <int d>
+void buildAnswer(const int l, const int r)
+{
+    const int len = d * (r - l) + 1;
+    if (len < 1)
+        return;
+    else if (len == 1)
+    {
+        shift(s.size() - s.find_last_of(t[l]) - 1);
+        shift(1);
+        return;
+    }
+    buildAnswer<(-d)>(r - d, l + d);
+    {
+        const unsigned int px = s.find_last_of(t[r]);
+        shift(s.length() - px);
+        if (px + 2 > len)
+            shift(px + 2 - len);
+    }
+    shift(s.length() - s.find(t[l]) - 1);
+    shift(1);
+}
+
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    unsigned int n;
+    cin >> n >> s >> t;
+    if (!checkAnswer(s, t))
+    {
+        cout << "-1\n";
+        return 0;
+    }
+    buildAnswer<1>(0, n - 1);
+    cout << ans.size() << "\n";
+    copy(ans.cbegin(), ans.cend(), ostream_iterator<unsigned int>(cout, " "));
+    cout.put('\n');
+    return 0;
+}
