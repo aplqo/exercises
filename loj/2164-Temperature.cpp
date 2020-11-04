8573fd88b9a81556c68b9567c24881084578db9e
aplqo <aplqo@outlook.com>
2020-11-04 22:55:12 +0800

    [loj] Add 8 problems: 2156,2160,2163,2164,2166,2167,2170,2171

---
+++ loj/2164-Temperature.cpp
@@ -0,0 +1,47 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <deque>
+#include <iostream>
+using namespace std;
+const unsigned int maxn = 1e6;
+
+struct Day
+{
+    int l, r;
+} d[maxn + 10];
+
+unsigned int solve(const unsigned int n)
+{
+    unsigned int ret = 0;
+    deque<Day*> q;
+    for (Day* i = d; i < d + n; ++i)
+    {
+        while (!q.empty() && q.front()->l > i->r)
+            q.pop_front();
+        if (!q.empty())
+            ret = max<unsigned int>(ret, i + 1 - q.front());
+        Day* lst = i;
+        while (!q.empty() && q.back()->l < i->l)
+        {
+            lst = q.back();
+            q.pop_back();
+        }
+        lst->l = i->l;
+        q.push_back(lst);
+    }
+    return ret;
+}
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    unsigned int n;
+    cin >> n;
+    for (Day* i = d; i < d + n; ++i)
+        cin >> i->l >> i->r;
+    cout << solve(n) << "\n";
+    return 0;
+}
