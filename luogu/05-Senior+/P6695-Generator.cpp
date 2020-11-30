2f92526f2c7939f4c66036418ab91ae192d6629f
aplqo <aplqo@outlook.com>
2020-11-30 22:44:42 +0800

    [luogu] Add 6 problems: P6682,P6695,P4198,P4798,P4799,P6698

---
+++ luogu/05-Senior+/P6695-Generator.cpp
@@ -0,0 +1,22 @@
+#include "debug_tools/generator.h"
+#include <algorithm>
+#include <cstring>
+#include <fstream>
+using namespace std;
+using namespace apdebug::generator;
+const int maxn = 1e5;
+
+void createTree(ofstream& in, const unsigned int n)
+{
+    for (unsigned int i = 2; i <= n; ++i)
+        in << i << " " << GetRandom(1u, i - 1) << "\n";
+}
+int main(int, char* argv[])
+{
+    Init();
+    const unsigned int n = GetRandom(2, MayBe(atoi(argv[1]), maxn));
+    ofstream in(argv[2]);
+    in << n << "\n";
+    createTree(in, n);
+    return 0;
+}
