f81abc5417022c8c03c26ae3d8566864b821cdf6
aplqo <aplqo@outlook.com>
2020-11-30 22:42:49 +0800

    [loj] Add 6 problem: 2787,2788,2789,3332,3336,3337

---
+++ loj/2787-CalvinballChampionship.cpp
@@ -0,0 +1,40 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+using namespace std;
+const unsigned int maxn = 10000;
+const unsigned int mod = 1000007;
+
+unsigned int y[maxn + 10];
+
+unsigned long long count(const unsigned int n)
+{
+    static unsigned long long f[maxn + 10];
+    static unsigned int mx[maxn + 10];
+    fill(f, f + 1 + n, 1);
+    partial_sum(y, y + n, mx, [](unsigned int a, unsigned int b) { return max(a, b); });
+    unsigned long long ret = 1;
+    for (int i = n - 2; i >= 0; --i)
+    {
+        ret = (ret + ((y[i + 1] - 1) * f[mx[i]]) % mod) % mod;
+        for (unsigned int j = 0; j <= n; ++j)
+            f[j] = ((f[j] * j) % mod + f[j + 1]) % mod;
+    }
+    return ret;
+}
+
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    unsigned int n;
+    cin >> n;
+    copy_n(istream_iterator<unsigned int>(cin), n, y);
+    cout << count(n) << "\n";
+    return 0;
+}
---
+++ loj/2789-IceHockeyChampionship.cpp
@@ -0,0 +1,61 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+using namespace std;
+const unsigned int maxn = 40, maxs = (1 << (maxn / 2));
+
+unsigned long long cost[maxn + 10];
+unsigned long long plan[2][maxs + 10];
+unsigned int lg[maxs + 10];
+
+static void getLog(const unsigned int n)
+{
+    for (unsigned int i = 0; i <= n; ++i)
+        lg[1 << i] = i;
+}
+#define lowbit(x) ((x) & -(x))
+void enumerate(const unsigned long long* begin, const unsigned int n, unsigned long long res[])
+{
+    const unsigned int msk = 1u << n;
+    for (unsigned int i = 1; i < msk; ++i)
+    {
+        const unsigned int lb = lowbit(i);
+        res[i] = res[i ^ lb] + begin[lg[lb]];
+    }
+}
+#undef lowbit
+
+unsigned long long count(const unsigned int n, const unsigned long long lim)
+{
+    const unsigned int pl = n / 2, pr = n - pl;
+    const unsigned int sizeL = 1u << pl, sizeR = 1u << pr;
+    getLog(max(pl, pr));
+    enumerate(cost, pl, plan[0]);
+    enumerate(cost + pl, pr, plan[1]);
+    sort(plan[0], plan[0] + sizeL);
+    sort(plan[1], plan[1] + sizeR);
+    const unsigned long long* ptr = plan[0] + sizeL - 1;
+    unsigned long long ret = 0;
+    for (const unsigned long long* i = plan[1]; i < plan[1] + sizeR; ++i)
+    {
+        while (ptr >= plan[0] && *ptr + (*i) > lim)
+            --ptr;
+        ret += ptr + 1 - plan[0];
+    }
+    return ret;
+}
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    unsigned int n;
+    unsigned long long lim;
+    cin >> n >> lim;
+    copy_n(istream_iterator<unsigned long long>(cin), n, cost);
+    cout << count(n, lim) << "\n";
+    return 0;
+}
