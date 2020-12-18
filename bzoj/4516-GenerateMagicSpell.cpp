a1a16436b634a8be671b61af6e025e6ca94597cc
aplqo <aplqo@outlook.com>
2020-12-18 11:43:25 +0800

    [bzoj] Add 5 problems: 3676,3998,4032,4516,4566

---
+++ bzoj/4516-GenerateMagicSpell.cpp
@@ -0,0 +1,72 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+const unsigned int maxn = 2e5;
+
+struct SAMNode
+{
+    SAMNode* link;
+    unordered_map<unsigned int, SAMNode*> next;
+    unsigned int len;
+} nodes[maxn * 2 + 10], root {};
+
+unsigned int insertChar(const unsigned int c)
+{
+    static SAMNode *cur = nodes, *last = &root;
+    SAMNode* const ptr = cur++;
+    new (ptr) SAMNode {};
+    ptr->len = last->len + 1;
+    SAMNode* from = last;
+    while (from && !from->next.count(c))
+    {
+        from->next[c] = ptr;
+        from = from->link;
+    }
+    if (!from)
+        ptr->link = &root;
+    else
+    {
+        SAMNode* const fNxt = from->next[c];
+        if (fNxt->len == from->len + 1)
+            ptr->link = fNxt;
+        else
+        {
+            SAMNode* const cloned = cur++;
+            *cloned = *fNxt;
+            cloned->len = from->len + 1;
+            for (SAMNode* i = from; i; i = i->link)
+            {
+                const auto it = i->next.find(c);
+                if (it != i->next.end() && it->second == fNxt)
+                    it->second = cloned;
+                else
+                    break;
+            }
+            fNxt->link = ptr->link = cloned;
+        }
+    }
+    last = ptr;
+    return ptr->len - ptr->link->len;
+}
+
+int main()
+{
+#ifndef APTEST
+    ios_base::sync_with_stdio(false);
+#endif
+    unsigned int n;
+    unsigned long long ans = 0;
+    cin >> n;
+    for (unsigned int i = 0; i < n; ++i)
+    {
+        unsigned int c;
+        cin >> c;
+        ans += insertChar(c);
+        cout << ans << "\n";
+    }
+    return 0;
+}
