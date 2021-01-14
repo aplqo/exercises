68463ac9da3e423e4b7ae5abfe6e2f4fbd54f6f7
aplqo <aplqo@outlook.com>
2021-01-14 21:18:01 +0800

    [loj] Add 5 problems: 3113,3159,3282,6008,6031

---
+++ loj/3159-Jump.cpp
@@ -0,0 +1,194 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <queue>
+#include <vector>
+using namespace std;
+const unsigned int maxn = 7e4, maxm = 1.5e5;
+constexpr unsigned int inf = UINT_MAX / 2;
+
+enum
+{
+    X = 0,
+    Y = 1
+};
+struct ValFrom
+{
+    unsigned int val, from;
+};
+struct ValGreater
+{
+    inline bool operator()(const ValFrom& l, const ValFrom& r) const
+    {
+        return l.val != r.val ? l.val > r.val : l.from < r.from;
+    }
+};
+priority_queue<ValFrom, vector<ValFrom>, ValGreater> heap;
+typedef array<unsigned int, 2> Point;
+struct Square
+{
+    Point l, r;
+
+    inline bool isInside(const Square& other) const
+    {
+        return other.l[X] <= l[X] && other.l[Y] <= l[Y] && r[X] <= other.r[X] && r[Y] <= other.r[Y];
+    }
+    inline bool isIntersect(const Square& other) const
+    {
+        return !(other.r[X] < l[X] || other.r[Y] < l[Y] || other.l[X] > r[X] || other.l[Y] > r[Y]);
+    }
+};
+struct Transition
+{
+    Square to;
+    unsigned int time;
+};
+unsigned int dis[maxn * 4 + 19], vcnt;
+bool vis[maxn * 4 + 10];
+struct City
+{
+    unsigned int id;
+    Point point;
+    vector<Transition> next;
+} city[maxn + 10], *ccur = city;
+namespace KDTree
+{
+    struct Node
+    {
+        unsigned int id;
+        Square space;
+        const Node *lc, *rc;
+
+        template <unsigned int typ>
+        void build(City** const l, City** const r);
+        void update(const Square& s, const unsigned int val) const;
+        inline void relaxation() const;
+        inline void pushup();
+    } nodes[maxn * 4 + 10], *cur = nodes, *root = nodes;
+    Node* revId[maxn * 5 + 10];
+
+    template <unsigned int typ>
+    void Node::build(City** const l, City** const r)
+    {
+        static const auto pred = [](City* a, City* b) {
+            return a->point[typ] < b->point[typ];
+        };
+        const unsigned int len = r - l, mid = len >> 1;
+        id = ++vcnt;
+        revId[id] = this;
+        if (len == 1)
+        {
+            id = (*l)->id;
+            space = Square { (*l)->point, (*l)->point };
+            return;
+        }
+        nth_element(l, l + mid, r, pred);
+        City** const pmid = l + mid;
+        lc = ++cur;
+        cur->build<!typ>(l, pmid);
+        rc = ++cur;
+        cur->build<!typ>(pmid, r);
+        pushup();
+    }
+    inline void Node::pushup()
+    {
+        space.l[X] = min(lc->space.l[X], rc->space.l[X]);
+        space.l[Y] = min(lc->space.l[Y], rc->space.l[Y]);
+        space.r[X] = max(lc->space.r[X], rc->space.r[X]);
+        space.r[Y] = max(lc->space.r[Y], rc->space.r[Y]);
+    }
+    void Node::update(const Square& s, const unsigned int val) const
+    {
+        if (!space.isIntersect(s))
+            return;
+        if (space.isInside(s))
+        {
+            if (dis[id] > val)
+            {
+                dis[id] = val;
+                heap.push(ValFrom { dis[id], id });
+            }
+            return;
+        }
+        lc->update(s, val);
+        rc->update(s, val);
+    }
+    inline void Node::relaxation() const
+    {
+        if (dis[lc->id] > dis[id])
+        {
+            dis[lc->id] = dis[id];
+            heap.push(ValFrom { dis[lc->id], lc->id });
+        }
+        if (dis[rc->id] > dis[id])
+        {
+            dis[rc->id] = dis[id];
+            heap.push(ValFrom { dis[rc->id], rc->id });
+        }
+    }
+    inline void build(City* const l, City* const r)
+    {
+        static City* buf[maxn + 10];
+        iota(buf, buf + (r - l), l);
+        root->build<X>(buf, buf + (r - l));
+    }
+}
+
+void dijkstra(const unsigned int n)
+{
+    fill(dis, dis + vcnt + 1, inf);
+    dis[0] = 0;
+    heap.push(ValFrom { .val = 0, .from = 0 });
+    while (!heap.empty())
+    {
+        const unsigned int cur = heap.top().from;
+        heap.pop();
+        if (vis[cur])
+            continue;
+        vis[cur] = true;
+        if (cur < n)
+        {
+            for (const auto& i : city[cur].next)
+                KDTree::root->update(i.to, i.time + dis[cur]);
+        }
+        else
+            KDTree::revId[cur]->relaxation();
+    }
+}
+
+int main()
+{
+#ifndef APTEST
+    ios::sync_with_stdio(false);
+#endif
+    ifstream is("jump.in");
+    ofstream os("jump.out");
+    is.tie(nullptr);
+    os.tie(nullptr);
+    unsigned int n, m, w, h;
+    is >> n >> m >> w >> h;
+    vcnt = n;
+    for (City* i = city; i < city + n; ++i)
+    {
+        is >> i->point[X] >> i->point[Y];
+        i->id = i - city;
+    }
+    for (unsigned int i = m; i; --i)
+    {
+        unsigned int p;
+        Transition tp;
+        is >> p >> tp.time >> tp.to.l[X] >> tp.to.r[X] >> tp.to.l[Y] >> tp.to.r[Y];
+        city[p - 1].next.push_back(tp);
+    }
+    KDTree::build(city, city + n);
+    dijkstra(n);
+    copy(dis + 1, dis + n, ostream_iterator<unsigned int>(os, "\n"));
+    return 0;
+}
---
+++ loj/3282-TreatmentProject.cpp
@@ -0,0 +1,177 @@
+#ifdef APTEST
+#include "debug_tools/program.h"
+#endif
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <queue>
+using namespace std;
+const unsigned int maxm = 1e5, maxl = 20;
+constexpr unsigned long long inf = ULLONG_MAX / 2;
+
+struct Plan
+{
+    int l, r, t;
+    int lx, ly, rx, ry;
+    unsigned int cost;
+    unsigned long long dist;
+    bool vis;
+} pl[maxm + 10];
+struct Node
+{
+    unsigned long long dist;
+    Plan* ptr;
+};
+struct NodeLess
+{
+    inline bool operator()(const Node l, const Node r) const
+    {
+        return l.dist != r.dist ? l.dist > r.dist : l.ptr < r.ptr;
+    }
+};
+priority_queue<Node, vector<Node>, NodeLess> q;
+class SegmentTree
+{
+public:
+    void build(const unsigned int l, const unsigned int r, Plan*(&p), Plan* const ed, SegmentTree*(&cur));
+    void updatePrefix(const Plan& p);
+
+private:
+    unsigned int lv, mv, rv;
+    SegmentTree *lc, *rc;
+    Plan **begin, **end;
+    static Plan *mem[maxm * maxl + 10], **cur;
+} nodes[maxm * 2 * 4 + 10], *root = nodes;
+Plan *SegmentTree::mem[maxm * maxl + 10], **SegmentTree::cur = SegmentTree::mem;
+
+void SegmentTree::build(const unsigned int l, const unsigned int r, Plan*(&p), Plan* const ed, SegmentTree*(&cur))
+{
+    static const auto pred = [](Plan* l, Plan* r) { return l->ly < r->ly; };
+    lv = l;
+    rv = r;
+    mv = (l + r) >> 1;
+    if (rv - lv == 1)
+    {
+        begin = SegmentTree::cur;
+        while (p < ed && p->lx == l)
+        {
+            if (!p->vis)
+                *(SegmentTree::cur++) = p;
+            ++p;
+        }
+        end = SegmentTree::cur;
+        sort(begin, end, pred);
+        return;
+    }
+    lc = ++cur;
+    lc->build(l, mv, p, ed, cur);
+    rc = ++cur;
+    rc->build(mv, r, p, ed, cur);
+    begin = SegmentTree::cur;
+    end = merge(lc->begin, lc->end, rc->begin, rc->end, begin, pred);
+    SegmentTree::cur = end;
+}
+void SegmentTree::updatePrefix(const Plan& p)
+{
+    if (begin == end)
+        return;
+    if (rv < p.rx || rv - lv == 1)
+    {
+        while (begin < end && (*begin)->ly <= p.ry)
+        {
+            if (!(*begin)->vis)
+            {
+                Plan* const ptr = *begin;
+                ptr->dist = p.dist + ptr->cost;
+                ptr->vis = true;
+                q.push(Node { ptr->dist, ptr });
+            }
+            ++begin;
+        }
+        return;
+    }
+    lc->updatePrefix(p);
+    if (mv <= p.rx)
+        rc->updatePrefix(p);
+}
+
+static void initPlan(const unsigned int m)
+{
+    for (Plan* i = pl; i < pl + m; ++i)
+    {
+        i->lx = i->l - i->t;
+        i->ly = i->l + i->t;
+        i->rx = i->r - i->t + 1;
+        i->ry = i->r + i->t + 1;
+        if (i->l == 1)
+        {
+            i->vis = true;
+            i->dist = i->cost;
+        }
+        else
+        {
+            i->vis = false;
+            i->dist = inf;
+        }
+    }
+}
+static int discretize(const unsigned int m)
+{
+    static int buf[maxm * 2 + 10];
+    int* cur = buf;
+    for (Plan* i = pl; i < pl + m; ++i)
+    {
+        *(cur++) = i->lx;
+        *(cur++) = i->rx;
+    }
+    sort(buf, cur);
+    cur = unique(buf, cur);
+    for (Plan* i = pl; i < pl + m; ++i)
+    {
+        i->lx = lower_bound(buf, cur, i->lx) - buf + 1;
+        i->rx = lower_bound(buf, cur, i->rx) - buf + 1;
+    }
+    return cur - buf + 1;
+}
+
+unsigned long long solve(const unsigned int m, const unsigned int n)
+{
+    initPlan(m);
+    {
+        SegmentTree* c = nodes;
+        Plan* p = pl;
+        sort(pl, pl + m, [](const Plan& l, const Plan& r) { return l.lx < r.lx; });
+        const int dcnt = discretize(m);
+        for (Plan* i = pl; i < pl + m; ++i)
+            if (i->vis)
+                q.push(Node { i->dist, i });
+        root->build(0, dcnt, p, pl + m, c);
+    }
+    unsigned long long ret = inf;
+    while (!q.empty())
+    {
+        const Plan* const cur = q.top().ptr;
+        q.pop();
+        root->updatePrefix(*cur);
+        if (cur->r == n)
+            ret = min(ret, cur->dist);
+    }
+    return ret;
+}
+
+int main()
+{
+#ifndef APTEST
+    ios::sync_with_stdio(false);
+#endif
+    unsigned int n, m;
+    cin >> n >> m;
+    for (Plan* i = pl; i < pl + m; ++i)
+        cin >> i->t >> i->l >> i->r >> i->cost;
+    const unsigned long long v = solve(m, n);
+    if (v == inf)
+        cout << "-1\n";
+    else
+        cout << v << "\n";
+    return 0;
+}
